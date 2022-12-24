bc1e948a54460dedafbfece10132c9a958ec7f96
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-24 21:23:51 +0530

    Create posorneg.java
    Program to find if the number is Positive or Negative.

---
+++ posorneg.java
@@ -0,0 +1,23 @@
+/*Aim:Program to find if the number is Positive or Negative.
+Description: Program to check whether the input given by user is positive or negative.*/
+
+//Program:
+public class PosOrNeg
+{
+    public static void main(String[] args)
+    {
+        int a=192;
+        if(a>0)
+        {
+            System.out.println("The number is positive.");
+        }
+        else if(a<0)
+        {
+            System.out.println("The number is negative.");
+        }
+        else
+        {
+            System.out.println("The number is zero.");
+        }
+    }
+}
