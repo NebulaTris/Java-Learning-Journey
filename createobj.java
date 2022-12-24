19be32745ed584e5050b43427debc76460a311ea
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-24 16:51:05 +0530

    Create createobj.java
    Program to create object of another class

---
+++ createobj.java
@@ -0,0 +1,15 @@
+/* Aim: Program to create object of another class 
+Description : A Java object is a member of a Java class. Each object has an identity ,a behvaiour and a state.*/
+
+class myfirstclass{
+    static int x;
+    public static void main(String args[]){
+        int x;
+        System.out.println("My First Class");
+        Student obj=new Student();
+    }
+    }
+class Student{
+    int rollno;
+    static int func_class;
+}
