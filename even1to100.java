b502a6190b2f203b419ed047b283de689ea5e4d5
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-24 21:31:54 +0530

    Update even1to100.java

--- even1to100.java
+++ even1to100.java
@@ -4,11 +4,11 @@ Description: Program that displays even number from 1 to 100 using "for" loop */
 //Program
 public class EvenNum
 {
-    public static void main (String[] args)
+    public static void main(String args[])
     {
-        int no=100;
-        System.out.print("List of even numbers from 1 to 100:");
-        for (int i=1;i<=no;i++)
+        int num=100;
+        System.out.print("List of even numbers from 1 to "+num+":");
+        for (int i=1;i<=num;i++)
         {
             if (i%2==0)
             {
