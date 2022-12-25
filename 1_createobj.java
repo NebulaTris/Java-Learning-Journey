b7c7d567285947b90b7853bed880930d1c48fce9
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2022-12-25 21:15:21 +0530

    Update 1_createobj.java

--- 1_createobj.java
+++ 1_createobj.java
@@ -1,6 +1,7 @@
 /* Aim: Program to create object of another class 
 Description : A Java object is a member of a Java class. Each object has an identity ,a behvaiour and a state.*/
 
+//Program
 class myfirstclass{
     static int x;
     public static void main(String args[]){
