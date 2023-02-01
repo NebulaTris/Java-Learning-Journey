4f51312890b15b66ffd3dd565edb6ec7ea53bf10
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-02-01 22:22:02 +0530

    /* To be continued later */

--- Basics/inhtest.java
+++ Basics/inhtest.java
@@ -1,1 +1,19 @@
 /* To be done*/
+import java.util.*;
+class student
+{
+  void getinfo()
+  {
+    Scanner sc=new Scanner(System.in);
+    System.out.print("Enter Roll No.:");
+    int rollno=sc.nextInt();
+    System.out.print("Enter Name:");
+    String name=sc.nextLine();
+  }
+  void putinfo()
+  {
+    System.out.print("Roll No.:" +rollno);
+    System.out.print("Name:" +name);
+  }
+}
+
