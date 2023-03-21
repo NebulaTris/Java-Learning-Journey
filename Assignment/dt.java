ff545573f084b5f1d8bc4b5d13cb25cc71d3b855
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-03-21 08:11:25 +0530

    Ques 7 - Assignment

---
+++ Assignment/dt.java
@@ -0,0 +1,16 @@
+//Aim: Write a Java program to get current full date and time
+
+//Code:
+import java.util.*;
+public class  dt{
+   public static void main(String[] args)
+    {
+   Calendar now = Calendar.getInstance();
+    System.out.println();
+    System.out.println("Current full date and time is : " + (now.get(Calendar.MONTH) + 1) + "-"
+        + now.get(Calendar.DATE) + "-" + now.get(Calendar.YEAR) + " "
+        + now.get(Calendar.HOUR_OF_DAY) + ":" + now.get(Calendar.MINUTE) + ":"
+        + now.get(Calendar.SECOND) + "." + now.get(Calendar.MILLISECOND));
+	System.out.println();
+    }
+}
