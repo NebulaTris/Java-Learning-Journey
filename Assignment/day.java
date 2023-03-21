d77d3397ba9125bf505604faf4e3e0a6de5de4c0
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-03-21 08:50:05 +0530

    Ques 8 - Assignment

--- Assignment/day.java
+++ Assignment/day.java
@@ -1,15 +1,57 @@
 //Aim: Write a Java program to get a day of the week of a specific date. 
 
 //Code:
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.*;
-public class day {
-public static void main(String []args){
 
-        Calendar cal = Calendar.getInstance();
+public class DayOfWeek {
+    
+    public static void main(String[] args) {
 
-	System.out.println();
-        int dayOfWeek = cal.get(Calendar.DAY_OF_WEEK);
-        System.out.println("Day of the week : " + dayOfWeek); 
-	System.out.println();
+        Scanner sc= new Scanner(System.in); 
+        System.out.print("Enter the date in format yyyy-MM-dd: ");  
+        String str= sc.nextLine();        
+
+        Date date = null;
+        try {
+            date = new SimpleDateFormat("yyyy-MM-dd").parse(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+        
+        String dayOfWeekString = "";
+        switch (dayOfWeek) {
+            case 1:
+                dayOfWeekString = "Sunday";
+                break;
+            case 2:
+                dayOfWeekString = "Monday";
+                break;
+            case 3:
+                dayOfWeekString = "Tuesday";
+                break;
+            case 4:
+                dayOfWeekString = "Wednesday";
+                break;
+            case 5:
+                dayOfWeekString = "Thursday";
+                break;
+            case 6:
+                dayOfWeekString = "Friday";
+                break;
+            case 7:
+                dayOfWeekString = "Saturday";
+                break;
+            default:
+                break;
+        }
+        System.out.println(dayOfWeekString);
     }
 }
