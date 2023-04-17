f2f0c698f7cfd4ba9bdb0c8859cabb893b8464f1
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-04-17 20:57:49 +0530

    --<<Semester Ends>>--

---
+++ Practical File 2/Exp11.java
@@ -0,0 +1,61 @@
+/*OBJECTIVE: Write a program to show the use of yield (), stop () and sleep ().
+
+SOFTWARE USED: NetBeans IDE*/
+
+//SOURCE CODE:
+package javaapplication24;
+
+import java.lang.Exception;
+class X implements Runnable
+{
+  public void run ()
+  {
+    for (int i = 1; i <= 10; i++)
+      {
+	System.out.println ("\tThread X : " + i);
+      }
+    System.out.println ("End of Thread X");
+  }
+}
+
+class Y implements Runnable
+{
+  public void run ()
+  {
+    for (int j = 1; j <= 10; j++)
+      {
+	System.out.println ("\t Thread  Y:" + j);
+      }
+    System.out.println ("End of Thread Y");
+  }
+}
+
+class Z implements Runnable
+{
+  public void run ()
+  {
+    for (int k = 1; k <= 10; k++)
+      {
+	System.out.println ("\tThread Z : " + k);
+      }
+    System.out.println ("End of Thread Z");
+  }
+}
+
+public class JavaApplication24
+{
+  public static void main (String args[])
+  {
+    X runnable = new X ();
+    Thread threadX = new Thread (runnable);
+    Y runnable1 = new Y ();
+    Thread threadY = new Thread (runnable1);
+    Z runnable2 = new Z ();
+    Thread threadZ = new Thread (runnable2);
+      threadX.start ();
+      threadY.start ();
+      threadZ.start ();
+
+      System.out.println ("End of main thread");
+  }
+}
